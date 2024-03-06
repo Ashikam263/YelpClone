7c886435883c50978cd84433d83de2c7406fc07e
Ashikam263 <ashikam263@gmail.com>
2024-03-06 17:38:47 +0530

    restarting

--- server/server.js
+++ server/server.js
@@ -1,16 +1,14 @@
 require('dotenv').config()
+const e = require('express');
 const express = require('express')
+const morgan = require('morgan');
 
 const app = express();
-
-app.use((req, res, next) => {
-  console.log('Middleware');
-  next();
-});
+app.use(express.json());
 
 //get all restaurants
 app.get('/api/v1/restaurants', (req, res) => {
-  console.log("route handeler ran for middle ware")
+  console.log("route handeler ran")
   res.status(200).json({
     status: 'success',
     data : {
@@ -32,7 +30,37 @@ app.get('/api/v1/restaurants/:id', (req, res) => {
 
 //create a restaurant
 app.post('/api/v1/restaurants', (req, res) => {
-  console.log(req);
+  console.log(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data : {
+      restaurant : 'mcdnalads'
+    }
+  })
+});
+
+//update a restaurant
+app.put('/api/v1/restaurants/:id', (req, res) => {
+  console.log(req.params.id);
+  console.log(req.body);
+
+  res.status(200).json({
+    status: 'success',
+    data : {
+      restaurant : 'mcdnalads'
+    }
+  })
+}); 
+
+//delete a restaurant
+app.delete('/api/v1/restaurants/:id', (req, res) => {
+  console.log(req.params.id);
+
+  res.status(204).json({
+    status: 'success',
+    data : null
+  })
 });
 
 const port = process.env.PORT || 3000;
