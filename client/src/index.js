60ce19f2c77fbc8372bbe1fbdc7d922fc10ea866
Ashikam263 <ashikam263@gmail.com>
2024-03-14 12:35:01 +0530

    list and pages not siplaying

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Find the element you want to attach your React app to.
+const container = document.getElementById('root');
+
+// Create a root.
+const root = createRoot(container);
+
+// Initial render: Render your app to the root.
+root.render(<App />);
