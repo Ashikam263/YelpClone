b905a1f5034b329deb1f44c24107289a8a37cbe6
Ashikam263 <ashikam263@gmail.com>
2024-03-23 00:56:23 +0530

    prettier

--- client/src/components/AddReview.jsx
+++ client/src/components/AddReview.jsx
@@ -28,45 +28,50 @@ const AddReview = () => {
   };
   return (
     <div className="mb-2">
-    <Form>
-      <Form.Group className="mb-3" controlId="name">
-        <Form.Label>Name</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="rating">
-        <Form.Label>Rating</Form.Label>
-        <Form.Select
-          value={rating}
-          onChange={(e) => setRating(e.target.value)}
+      <Form>
+        <Form.Group className="mb-3" controlId="name">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="rating">
+          <Form.Label>Rating</Form.Label>
+          <Form.Select
+            value={rating}
+            onChange={(e) => setRating(e.target.value)}
+          >
+            <option disabled>Rating</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </Form.Select>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="Review">
+          <Form.Label>Review</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            value={reviewText}
+            onChange={(e) => setReviewText(e.target.value)}
+          />
+        </Form.Group>
+        <Button
+          variant="secondary"
+          type="submit"
+          onClick={handleSubmitReview}
+          className="w-100"
         >
-          <option disabled>Rating</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-        </Form.Select>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="Review">
-        <Form.Label>Review</Form.Label>
-        <Form.Control
-          as="textarea"
-          rows={3}
-          value={reviewText}
-          onChange={(e) => setReviewText(e.target.value)}
-        />
-      </Form.Group>
-      <Button variant="secondary" type="submit" onClick={handleSubmitReview} className="w-100">
-        Submit
-      </Button>
-    </Form>
-  </div>
-);
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 };
 
 export default AddReview;
